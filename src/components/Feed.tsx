bc148d9a0bf9584a2c8baf69b4553958dc3f9f56
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2024-12-25 15:07:25 +0900

    home-ui

---
+++ src/components/Feed.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import { FaAngleUp, FaCommentDots } from "react-icons/fa";
+
+export default function Feed() {
+	return (
+		<Link
+			className="flex justify-between bg-white shadow-md p-6 rounded-lg"
+			to={`/feeds/1`}
+		>
+			<div>
+				<button className="p-3 bg-gray-100 rounded-lg text-sm flex flex-col items-center gap-1 text-blue-950">
+					<FaAngleUp className="text-xs text-center font-bold" />
+					<div className="font-bold">12</div>
+				</button>
+			</div>
+			<div className="flex-1 px-10 min-w-0 flex flex-col gap-4">
+				<div className="flex flex-col">
+					<h2 className="text-gray-900 text-lg font-bold">제목입니다.</h2>
+					<p className="text-gray-600 truncate text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque impedit tenetur itaque vero dolore. Voluptatem voluptatibus recusandae sed maiores tenetur commodi, itaque fuga animi, eum magni nemo qui! Saepe, perspiciatis!</p>
+				</div>
+				<p className="text-right text-xs text-gray-600">
+					작성일: {new Date().toLocaleDateString()}
+				</p>
+			</div >
+
+			<div className="flex items-center gap-1 p-3 text-gray-600">
+				<FaCommentDots className="font-bold text-xl" />
+				<div className="font-bold">12</div>
+			</div>
+		</Link>
+	)
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+import Feed from "../components/Feed";
+
 export default function Home() {
-	return <div>Home</div>;
+	return (
+		<>
+			<div className="flex justify-between items-center mb-6">
+				<h1 className="text-2xl font-bold">글 목록</h1>
+				<Link to="/feeds/create" className="bg-blue-500 text-white px-4 py-2 rounded-md">
+					글쓰기
+				</Link>
+			</div>
+			<div className="flex flex-col gap-4 mb-10">
+				<Feed />
+				<Feed />
+				<Feed />
+				<Feed />
+				<Feed />
+			</div >
+		</>
+	);
 }
